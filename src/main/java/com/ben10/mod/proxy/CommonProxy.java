45cae4e365dee92cb1b6aa7e5adf9e05519495c5
zacn01 <32979901+zacn01@users.noreply.github.com>
2017-10-22 21:29:08 +0200

    Create CommonProxy.java

---
+++ src/main/java/com/ben10/mod/proxy/CommonProxy.java
@@ -0,0 +1,16 @@
+package com.bedrockminer.tutorial;
+
+public class CommonProxy {
+
+    public void preInit(FMLPreInitializationEvent e) {
+
+    }
+
+    public void init(FMLInitializationEvent e) {
+
+    }
+
+    public void postInit(FMLPostInitializationEvent e) {
+
+    }
+}
