477f87bdb3cf4895754eb84e95f2fa63cccf6481
zacn01 <32979901+zacn01@users.noreply.github.com>
2017-10-22 20:58:53 +0200

    Update

---
+++ src/main/java/com/example/examplemod/Ben10Mod.java
@@ -0,0 +1,19 @@
+package com.ben10.mod;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+
+@Mod(modid = Ben10Mod.MODID, version = Ben10Mod.VERSION)
+public class Ben10Mod
+{
+    public static final String MODID = "ben10";
+    public static final String VERSION = "a0.1";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    }
+}
