6aed1760f9a2ff6531494e5e85f14e63f938d8eb
zacn01 <32979901+zacn01@users.noreply.github.com>
2017-10-22 21:37:19 +0200

    Create ServerProxy.java

---
+++ src/main/java/com/ben10/mod/proxy/ServerProxy.java
@@ -0,0 +1,49 @@
+package com.ben10.mod.proxy;
+
+
+
+import com.ben10.mod.proxy.CommonProxy;
+
+import net.minecraftforge.common.event.FMLInitializationEvent;
+
+import net.minecraftforge.common.event.FMLPreInitializationEvent;
+
+import net.minecraftforge.common.event.FMLPostInitializationEvent;
+
+
+
+public class ServerProxy extends CommonProxy {
+
+
+
+        @Override
+
+        public void preInit(FMLPreInitializationEvent e) {
+
+        super.preInit(e);
+
+    }
+
+
+
+    @Override
+
+    public void init(FMLInitializationEvent e) {
+
+        super.init(e);
+
+    }
+
+
+
+    @Override
+
+    public void postInit(FMLPostInitializationEvent e) {
+
+        super.postInit(e);
+
+    }
+
+
+
+}
